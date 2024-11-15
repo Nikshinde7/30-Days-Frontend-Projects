8f163e30a89593a4fe59f96c61a8210ca09ed36d
Nikshinde7 <shindenikhil336@gmail.com>
2024-11-15 12:52:28 +0530

    fix: Added some fixes for the layout

--- src/app/day-4/page.tsx
+++ src/app/day-4/page.tsx
@@ -28,13 +28,13 @@ const links: TLink[] = [
 
 export default function Page() {
     return (
-        <div className="flex items-center justify-center h-screen w-full bg-[#141414] font-inter">
+        <div className="flex items-center justify-center h-full min-h-screen w-full bg-[#141414] font-inter py-10">
             <div className="p-10 bg-[#1f1f1f] h-[767px] w-[482px] rounded-2xl flex flex-col">
                 <div className=" w-full flex justify-center">
                     <Image src="/assets/avatar-jessica.jpeg" alt="img" width={112} height={112} className="rounded-full m-3" />
                 </div>
                 <div className="flex flex-col p-6 gap-y-4 items-center">
-                    <div className="text-3xl text-center font-semibold">
+                    <div className="text-3xl text-center font-semibold text-white">
                         Jessica Randall
                     </div>
                     <div className="text-[#bfde5c] text-lg font-bold">
